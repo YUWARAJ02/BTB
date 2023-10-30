16501d953f99d6a6ce1815bf7ffb3eef29be7e5a
Madhan Raj <madhansekar23@gmail.com>
2023-10-30 19:11:24 +0530

    done

--- front-end/BTB-front-end/BTB-react-app/src/App.js
+++ front-end/BTB-front-end/BTB-react-app/src/App.js
@@ -1,23 +1,26 @@
-import { Routes, Route } from "react-router-dom";
-import TrainHome from "./pages/TrainHome";
-import FlightPage from "./pages/FlightPage";
-import WhyUsPage from "./pages/WhyUsPage";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import TrainHome from './pages/TrainHome';
+import FlightPage from './pages/FlightPage';
+import WhyUsPage from './pages/WhyUsPage';
 import Login from './components/Loginsignup';
 import Signup from './components/signup';
-import NavigationBar from "./components/NavigationBar";
+import NavigationBar from './components/NavigationBar';
 
-
-function App() {
-  // ... (existing code)
-
+const App = () => {
   return (
     <div>
-      <TrainHome/>
+        
+      <Routes>
+        <Route path="/" element={<TrainHome />} />
+        <Route path="/flight" element={<FlightPage />} />
+        <Route path="/why-us" element={<WhyUsPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
       <NavigationBar />
-    
-
     </div>
   );
-}
+};
 
 export default App;
