ecd316b373574daf596dad53953de73063c7e2f0
Madhan Raj <madhansekar23@gmail.com>
2023-10-31 18:10:11 +0530

    done

--- front-end/BTB-front-end/BTB-react-app/src/components/Loginsignup.jsx
+++ front-end/BTB-front-end/BTB-react-app/src/components/Loginsignup.jsx
@@ -2,22 +2,16 @@ import React, { useState } from 'react';
 import './Loginsignup.css';
 import email_icon from "./Assets/email.png";
 import password_icon from "./Assets/password.png";
-import { Link } from 'react-router-dom';
-import Users from './API_request/User';
 import Signup from './signup';
 
-const handleSignUp = () => {
-    // Redirect logic can go here if needed
-}
-
-const Login = () => {
+const Loginsignup = () => {
     const [action, setAction] = useState("Login");
     const [formData, setFormData] = useState({
         email: '',
         password: '',
     });
     const [emailError, setEmailError] = useState('');
-    const [signinpop, signin]=useState(false);
+    const [showSignup, setShowSignup] = useState(false);
 
     const handleLogin = async () => {
         if (!validateEmail(formData.email)) {
@@ -52,56 +46,50 @@ const Login = () => {
         return emailPattern.test(email);
     }
 
-    if (!signinpop){
-        return (
+    return (
         <form onSubmit={(e) => {
             e.preventDefault();
             handleLogin();
         }}>
-            <div className='container'>
-                <div className="header">
-                    <div className='text'>{action}</div>
-                    <div className='underline'></div>
-                </div>
-                <div className='inputs'>
-                    <div className='input'>
-                        <img src={email_icon} alt='' />
-                        <input
-                            type="email"
-                            name="email"
-                            placeholder='Email Id'
-                            value={formData.email}
-                            onChange={handleChange}
-                        />
-                        {emailError && <div className="error-message">{emailError}</div>}
+            <div className='background'>
+                <div className='container'>
+                    <div className="header">
+                        <div className='text'>{action}</div>
+                        <div className='underline'></div>
                     </div>
-                    <div className='input'>
-                        <img src={password_icon} alt='' />
-                        <input
-                            type="password"
-                            name="password"
-                            placeholder='Password'
-                            value={formData.password}
-                            onChange={handleChange}
-                        />
-                        <span className="forgot-password" onClick={handleForgotPassword}>Forgot Password ?</span>
-                        {/* <Link to="/signup" className="signup-text">New user!</Link> */}
-                        <div className='signup-text' onClick={()=>{signin(true);console.log(signinpop)}} >New user!</div>
+                    <div className='inputs'>
+                        <div className='input'>
+                            <img src={email_icon} alt='' />
+                            <input
+                                type="email"
+                                name="email"
+                                placeholder='Email Id'
+                                value={formData.email}
+                                onChange={handleChange}
+                            />
+                            {emailError && <div className="error-message">{emailError}</div>}
+                        </div>
+                        <div className='input'>
+                            <img src={password_icon} alt='' />
+                            <input
+                                type="password"
+                                name="password"
+                                placeholder='Password'
+                                value={formData.password}
+                                onChange={handleChange}
+                            />
+                            <span className="forgot-password" onClick={handleForgotPassword}>Forgot Password ?</span>
+                            <div className='signup-text' onClick={() => setShowSignup(true)}>New user!</div>
+                        </div>
                     </div>
-                </div>
-                <div className="submit-container">
-                    <button type="submit" className={action === 'Login' ? 'submit gray' : "submit"}>Login</button>
+                    <div className="submit-container">
+                        <button type="submit" className={action === 'Login' ? 'submit gray' : "submit"}>Login</button>
+                    </div>
                 </div>
             </div>
+            {showSignup && <Signup />}
         </form>
     )
-    if(signinpop) {
-        console.log(signinpop)
-        return(
-        <><Signup/></>
-    )
-        }
-    }
 }
 
-export default Login;
+export default Loginsignup;
