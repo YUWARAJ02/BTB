f07d47b25d73d562cd0cf8cf2984e2dac859f2dc
Madhan Raj <madhansekar23@gmail.com>
2023-11-05 22:49:08 +0530

    changes

--- front-end/BTB-front-end/BTB-react-app/src/App.js
+++ front-end/BTB-front-end/BTB-react-app/src/App.js
@@ -3,13 +3,9 @@ import NavigationBar from "./components/NavigationBar";
 import {Routes,Route} from "react-router-dom";
 import "./pages/global.css"
 import React from 'react';
-import Header from "./components/Header";
 import TrainHome from "./pages/TrainHome";
-<<<<<<< HEAD
 import Forgetpassword from "./components/Forgetpassword";
-=======
-import BusHome from "./pages/BusHome";
->>>>>>> 1620fe499730c2ea71936bec2e753e591979f3d2
+
 
 
 
@@ -25,19 +21,12 @@ function App() {
     
     <div>
       <NavigationBar/>
-<<<<<<< HEAD
       <Routes>
     <Route path="/" element={<TrainHome/>}/>
     <Route path="/forgetpassword" element={<Forgetpassword/>}/>
     </Routes>
-=======
-      <BusHome/>
->>>>>>> 1620fe499730c2ea71936bec2e753e591979f3d2
-   </div>
     
-  
-
-   
+   </div>   
   );
 }
 export default App ;
